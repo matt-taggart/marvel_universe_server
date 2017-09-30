45ec84dec1caa6726ef586b95eb749d7b014f3f5
Matthew Taggart <matthew.taggart@cardcash.com>
2017-09-30 13:45:31 -0400

    Added simple login route.

--- server.js
+++ server.js
@@ -19,11 +19,38 @@ router
   });
 
 router
+  .post('/login', async ctx => {
+    const { username, password } = ctx.request.body;
+
+    const { rowCount, rows: [user] } = await pool.query({
+      text: 'SELECT * FROM users WHERE username = ($1)',
+      values: [username],
+    });
+
+    if (!rowCount) {
+      ctx.status = 400;
+      ctx.body = Boom.badRequest('Username does not exist');
+    }
+
+    const isValid = await bcrypt.compare(password, user.password);
+
+    if (!isValid) {
+      ctx.status = 401;
+      ctx.body = Boom.unauthorized('Invalid password');
+    }
+
+    ctx.body = {
+      id: user.id,
+      username: user.username,
+    };
+  });
+
+router
   .post('/users', async ctx => {
     const { username, password } = ctx.request.body;
     const hashedPassword = await bcrypt.hash(password, 10);
 
-    const { rowCount, rows: [row] } = await pool.query({
+    const { rowCount, rows: [user] } = await pool.query({
       text: 'INSERT INTO users (username, password) VALUES ($1, $2) RETURNING id, username',
       values: [username, hashedPassword],
     });
@@ -34,9 +61,9 @@ router
       return;
     }
 
-    ctx.body = { 
-      id: row.id,
-      username: row.username,
+    ctx.body = {
+      id: user.id,
+      username: user.username,
     };
   });
 
