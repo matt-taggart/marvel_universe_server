15e82e5078f78df49ae6081adc089fa6281c392e
Matthew Taggart <matthew.taggart@cardcash.com>
2017-10-08 23:01:09 -0400

    Create test files for routes.

--- test/routes/creators.js
+++ test/routes/creators.js
@@ -3,10 +3,10 @@ const { assert } = require('chai');
 
 const agent = request.agent('http://localhost:3000');
 
-describe('Character Endpoints', function() {
-  it('Should get all characters from Marvel API', function(done) {
+describe.only('Creator Endpoints', function() {
+  it('Should get all creators from Marvel API', function(done) {
     agent
-      .get('/characters')
+      .get('/creators')
       .expect('Content-Type', /json/)
       .expect(200)
       .end(function(err, response) {
@@ -15,8 +15,11 @@ describe('Character Endpoints', function() {
         assert.isArray(data);
         assert.containsAllKeys(data[0], [
           'id',
-          'name',
-          'description',
+          'firstName',
+          'middleName',
+          'lastName',
+          'suffix',
+          'fullName',
           'modified',
           'thumbnail',
           'resourceURI',
@@ -30,9 +33,9 @@ describe('Character Endpoints', function() {
       });
   });
 
-  it('Should get individual character from Marvel API', function(done) {
+  it('Should get individual creator from Marvel API', function(done) {
     agent
-      .get('/characters/1011334')
+      .get('/creators/2289')
       .expect('Content-Type', /json/)
       .expect(200)
       .end(function(err, response) {
@@ -41,8 +44,11 @@ describe('Character Endpoints', function() {
         assert.isArray(data);
         assert.containsAllKeys(data[0], [
           'id',
-          'name',
-          'description',
+          'firstName',
+          'middleName',
+          'lastName',
+          'suffix',
+          'fullName',
           'modified',
           'thumbnail',
           'resourceURI',
@@ -56,84 +62,9 @@ describe('Character Endpoints', function() {
       });
   });
 
-  it('Should get all comics for an individual character from Marvel API', function(done) {
+  it('Should get all events for an individual creator from Marvel API', function(done) {
     agent
-      .get('/characters/1011334/comics')
-      .expect('Content-Type', /json/)
-      .expect(200)
-      .end(function(err, response) {
-        if (err) return done(err);
-        const data = response.body.data;
-        assert.isArray(data);
-        assert.containsAllKeys(data[0], [
-          'id',
-          'digitalId',
-          'title',
-          'issueNumber',
-          'variantDescription',
-          'description',
-          'modified',
-          'isbn',
-          'upc',
-          'diamondCode',
-          'ean',
-          'issn',
-          'format',
-          'pageCount',
-          'textObjects',
-          'resourceURI',
-          'urls',
-          'series',
-          'variants',
-          'collections',
-          'collectedIssues',
-          'dates',
-          'prices',
-          'thumbnail',
-          'images',
-          'creators',
-          'characters',
-          'stories',
-          'events'
-        ]);
-        done();
-      });
-  });
-
-  it('Should get all events for an individual character from Marvel API', function(done) {
-    agent
-      .get('/characters/1011334/events')
-      .expect('Content-Type', /json/)
-      .expect(200)
-      .end(function(err, response) {
-        if (err) return done(err);
-        const data = response.body.data;
-        assert.isArray(data);
-        assert.containsAllKeys(data[0], [
-          'id',
-          'title',
-          'description',
-          'resourceURI',
-          'urls',
-          'modified',
-          'start',
-          'end',
-          'thumbnail',
-          'creators',
-          'characters',
-          'stories',
-          'comics',
-          'series',
-          'next',
-          'previous'
-        ]);
-        done();
-      });
-  });
-
-  it('Should get all series for an individual character from Marvel API', function(done) {
-    agent
-      .get('/characters/1011334/series')
+      .get('/creators/2289/series')
       .expect('Content-Type', /json/)
       .expect(200)
       .end(function(err, response) {
@@ -164,9 +95,42 @@ describe('Character Endpoints', function() {
       });
   });
 
-  it('Should get all stories for an individual character from Marvel API', function(done) {
+  it('Should get all series for an individual creator from Marvel API', function(done) {
     agent
-      .get('/characters/1011334/stories')
+      .get('/creators/2289/series')
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .end(function(err, response) {
+        if (err) return done(err);
+        const data = response.body.data;
+        assert.isArray(data);
+        assert.containsAllKeys(data[0], [
+          'id',
+          'title',
+          'description',
+          'resourceURI',
+          'urls',
+          'startYear',
+          'endYear',
+          'rating',
+          'type',
+          'modified',
+          'thumbnail',
+          'creators',
+          'characters',
+          'stories',
+          'comics',
+          'events',
+          'next',
+          'previous'
+        ]);
+        done();
+      });
+  });
+
+  it('Should get all stories for an individual creator from Marvel API', function(done) {
+    agent
+      .get('/creators/2289/stories')
       .expect('Content-Type', /json/)
       .expect(200)
       .end(function(err, response) {
@@ -191,4 +155,5 @@ describe('Character Endpoints', function() {
         done();
       });
   });
+
 });
